732da54dfda526ee18bbd11c5d3c366f3264767d
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-08 16:47:51 +0100

    added start of function "palindromeCheck"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log("JS-Ready");
+// Chiedo all'utente una parola
+let chosenWord = prompt("Inserisci una parola", "lol").trim().toLowerCase();
+console.log(chosenWord);
+
+// Creo la mia funzione per verificare se la parola è palindroma
+function palindromeCheck() {
+  // Divido la parola in lettere e le inverto
+  let reversedChosenWord = chosenWord.split("");
+  console.log(reversedChosenWord);
+}
