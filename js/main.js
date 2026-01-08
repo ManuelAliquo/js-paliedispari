3d53be5d2259d8b3e131ffe20b02d087550eef98
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-08 18:02:38 +0100

    added verification function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 /*
 
 // ### Palidroma ###
-// Chiedere all’utente di inserire una parola.
-// Creare una funzione per capire se la parola inserita è palindroma.
 
 // Chiedo all'utente una parola
 let chosenWord = prompt("Inserisci una parola", "lol").trim().toLowerCase();
@@ -38,18 +36,14 @@ palindromeCheck(chosenWord);
 */
 
 // ### Pari e Dispari ###
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random da 1 a 5 per il PC usando una funzione.
-// Stabiliamo se la somma dei due numeri è pari o dispari usando una funzione.
-// Dichiariamo chi ha vinto.
 
 // Chiedo all'utente se sceglie Pari o Dispari
-let userChoice = prompt(`Scegli tra "Pari" o "Dispari"`, "pari").trim().toLowerCase();
+let userChoice = prompt(`Scegli tra "Pari" o "Dispari"`, "dispari").trim().toLowerCase();
 console.log("userChoice:", userChoice);
 
 // Chiedo all'utente un numero da 1 a 5
 let userNumber = parseInt(prompt("Scegli un numero da 1 a 5", 3));
-console.log("userNumber", userNumber);
+console.log("userNumber:", userNumber);
 
 // Dichiaro una funzione per generare il numero randomico del pc
 function randomNumberGenerator() {
@@ -59,3 +53,18 @@ function randomNumberGenerator() {
 // Invoco la funzione
 let pcNumber = randomNumberGenerator();
 console.log("pcNumber:", pcNumber);
+
+// Sommo i due numeri
+let numbersSum = userNumber + pcNumber;
+console.log("numbersSum:", numbersSum);
+
+// Dichiaro una funzione per verificare se la somma è pari o dispari
+function isSumEven() {
+  let isSumEven = numbersSum % 2 === 0;
+  isSumEven
+    ? console.log("isSumEven:", isSumEven) + alert("Sum is Even")
+    : console.log("isSumEven:", isSumEven) + alert("Sum is Odd");
+}
+
+// Invoco la funzione
+isSumEven();
