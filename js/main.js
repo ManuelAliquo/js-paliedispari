d514a07c609e053fde6e2b489ae492152d58295e
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-08 19:03:09 +0100

    added output and fixed function

--- js/main.js
+++ js/main.js
@@ -38,11 +38,11 @@ palindromeCheck(chosenWord);
 // ### Pari e Dispari ###
 
 // Chiedo all'utente se sceglie Pari o Dispari
-let userChoice = prompt(`Scegli tra "Pari" o "Dispari"`, "dispari").trim().toLowerCase();
+let userChoice = prompt(`Scegli tra "Pari" o "Dispari"`).trim().toLowerCase();
 console.log("userChoice:", userChoice);
 
 // Chiedo all'utente un numero da 1 a 5
-let userNumber = parseInt(prompt("Scegli un numero da 1 a 5", 3));
+let userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
 console.log("userNumber:", userNumber);
 
 // Dichiaro una funzione per generare il numero randomico del pc
@@ -53,18 +53,26 @@ function randomNumberGenerator() {
 // Invoco la funzione
 let pcNumber = randomNumberGenerator();
 console.log("pcNumber:", pcNumber);
+alert(`pcNumber: ${pcNumber}`);
 
 // Sommo i due numeri
 let numbersSum = userNumber + pcNumber;
 console.log("numbersSum:", numbersSum);
+alert(`numbersSum: ${numbersSum}`);
 
 // Dichiaro una funzione per verificare se la somma è pari o dispari
-function isSumEven() {
-  let isSumEven = numbersSum % 2 === 0;
-  isSumEven
-    ? console.log("isSumEven:", isSumEven) + alert("Sum is Even")
-    : console.log("isSumEven:", isSumEven) + alert("Sum is Odd");
+let isSumEven;
+
+function sumCheck() {
+  isSumEven = numbersSum % 2 === 0;
+  console.log("isSumEven:", isSumEven);
+  isSumEven ? alert("Sum is Even") : alert("Sum is Odd");
 }
 
 // Invoco la funzione
-isSumEven();
+sumCheck();
+
+// Dichiaro il vincitore
+let userWon = (isSumEven && userChoice === "pari") || (!isSumEven && userChoice === "dispari");
+userWon ? alert("User Wins") : alert("PC Wins");
+console.log("userWon:", userWon);
