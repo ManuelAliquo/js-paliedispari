32eafdc1b69774b9a045af5639f79eb4005ed0a7
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-01-08 17:01:58 +0100

    added "letter inverter" inside function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
 // Chiedo all'utente una parola
-let chosenWord = prompt("Inserisci una parola", "lol").trim().toLowerCase();
+let chosenWord = "nerd";
+//prompt("Inserisci una parola", "lol").trim().toLowerCase();
 console.log(chosenWord);
 
 // Creo la mia funzione per verificare se la parola è palindroma
 function palindromeCheck() {
   // Divido la parola in lettere e le inverto
-  let reversedChosenWord = chosenWord.split("");
-  console.log(reversedChosenWord);
+  let splittedWord = chosenWord.split("");
+  console.log(splittedWord);
+
+  // Dichiaro l'array per la parola invertita
+  const reversedWord = [];
+
+  // Inserisco ogni lettera nell'array
+  for (let i = 0; i < chosenWord.length; i++) {
+    let currentChar = chosenWord[i];
+    reversedWord.unshift(currentChar);
+  }
+
+  console.log(reversedWord);
 }
+
+// Invoco la funzione per verificare
+palindromeCheck(chosenWord);
